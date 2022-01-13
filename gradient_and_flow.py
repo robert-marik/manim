240c2b8daca5f653391f1ff531812cec69509c88
Robert Mařík <marik@mendelu.cz>
2022-01-13 21:46:02 +0100

    tok

--- gradient_and_flow.py
+++ gradient_and_flow.py
@@ -8,6 +8,7 @@ from common_definitions import *
 import os
 
 animation_runtime = 15
+wait_time = 5
 
 # from https://favpng.com/png_view/wood-frame-wood-circle-png/Kts40pG2
 wood_img = r"c:\Users\marik\Documents\GitHub\manim\wood"
@@ -22,6 +23,12 @@ def function(x,y):
     X = a*(x+b)
     Y = A*(y+B)
     return(-(Y**2 + X))
+
+def contour_x(y,C):
+    Y = A*(y+B)
+    X = - (Y**2+C)
+    x = X/a-b
+    return x
 
 def gradient(x,y,z=0):
     X = a*(x+b)
@@ -51,11 +58,11 @@ class Flow(MovingCameraScene):
         self.play(GrowFromCenter(title))
         self.play(GrowFromCenter(autor[0]))
         self.play(GrowFromCenter(autor[1]))
-        self.wait(10)
+        self.wait(wait_time*2)
 
 
         number_plane = NumberPlane()
-        self.play(ReplacementTransform(VGroup(title,autor),number_plane))
+        self.play(FadeOut(VGroup(title,autor)))
 
         wood_obj = ImageMobject(wood_img)
 
@@ -69,10 +76,26 @@ class Flow(MovingCameraScene):
         gradient_min = np.min(gradienty)
 
         number_of_contours = 20
-        countours = VGroup(*[
+        contours = VGroup(*[
             ImplicitFunction(lambda x,y:function(x,y)-i)            
-            .set_color(temperature_to_color(i, dolni_mez,horni_mez)) for i in np.linspace(dolni_mez + 0.05*(horni_mez-dolni_mez), horni_mez ,number_of_contours)]
+            .set_color(temperature_to_color(i, dolni_mez,horni_mez)) for i in np.linspace(dolni_mez + 0.05*(horni_mez-dolni_mez), horni_mez ,number_of_contours)
+            ]
         )
+
+        function_colors = VGroup(
+            *[
+              Square(side_length=0.1, fill_opacity=1).move_to((i,j,0)).set_color(temperature_to_color(function(i,j), dolni_mez,horni_mez))
+              for i in np.linspace(-7,7,150) for j in np.linspace(-4,4,80)  
+            ]
+        )
+
+        self.add(function_colors)
+        self.wait(wait_time*7)
+        self.add(contours, number_plane)
+        self.play(AnimationGroup(
+            FadeOut(function_colors),
+        ), run_time=5)
+        self.wait(wait_time*7)
 
         vectors_unscaled_opposite = ArrowVectorField(lambda p:-gradient(*p)[0]*RIGHT-gradient(*p)[1]*UP,
             x_range=[-7,7,1],
@@ -109,21 +132,17 @@ class Flow(MovingCameraScene):
         funkce = VGroup(MathTex(r"f=20-y^2-x"),MathTex(r"-\nabla f = \begin{bmatrix} 1\\2y\end{bmatrix}"))
         funkce.arrange(DOWN,aligned_edge=LEFT).to_corner(UL).add_background_rectangle(opacity=0.9, buff=0.5).set_z_index(10)
 
-        self.add(funkce)
-        self.wait()
-
-        self.play(AnimationGroup(*[Create(_) for _ in countours], lag_ratio=0.1))
-        self.wait()
-
         self.play(AnimationGroup(*[GrowArrow(_) for _ in vectors_unscaled_opposite], lag_ratio=0.05, run_time=2))
-        self.wait(8)
+        self.wait(5*wait_time)
         self.play(ReplacementTransform(vectors_unscaled_opposite,vectors_unscaled))
 
-        self.wait()
+        self.wait(3*wait_time)
         self.play(ReplacementTransform(vectors_unscaled,vectors))
-        self.wait()
+        self.wait(wait_time)
+        self.add(funkce)
+        self.wait(wait_time)
         self.add(curves)
-        self.wait()
+        self.wait(wait_time)
 
         vlastni_vektory=VGroup(*[vectors[3+9*i] for i in range(14)])
         surroundingRectangle= SurroundingRectangle(vlastni_vektory[1:-1],color=YELLOW, buff=0.15)
@@ -131,9 +150,7 @@ class Flow(MovingCameraScene):
         roh2 = vlastni_vektory[0].get_corner(UR)
         roh1 = vlastni_vektory[0].get_corner(DL)
         rozmery = roh2-roh1
-        print(rozmery)
         uhel = np.arctan(rozmery[1]/rozmery[0])/np.pi*180
-        print("Uhel pootoceni",uhel)
 
         obrazek = wood_obj.copy().rotate(uhel*DEGREES).scale_to_fit_width(3)
         obrazek.to_edge(RIGHT).shift(.7*DOWN).set_z_index(-1)
@@ -144,11 +161,9 @@ class Flow(MovingCameraScene):
         C,S = np.cos(uhel*DEGREES),np.sin(uhel*DEGREES)
         R = np.array([[C,S],[-S,C]])
         D = np.matmul(np.linalg.inv(R),np.matmul(tempD,R))
-        print(D)
         d11 = round(D[0,0],2)
         d12 = round(D[0,1],2)
         d22 = round(D[1,1],2)
-        print(d11,d12,d22)
 
         gradienty_D = [gradient_delka_D(x,y,d11=d11,d12=d12,d22=d22) for x in np.linspace(-7,7,10) for y in np.linspace(-4,4,10)]
         gradient_max_D = np.max(gradienty_D)
@@ -170,7 +185,7 @@ class Flow(MovingCameraScene):
             lambda p:(d11*gradient(*p)[0]+d12*gradient(*p)[1])*RIGHT+(d12*gradient(*p)[0]+d22*gradient(*p)[1])*UP,
             stroke_width=3,
             dt=0.5,
-            virtual_time=3, opacity=0.75
+            virtual_time=1.5, opacity=0.75
             ).set_z_index(-1)
 
         matice_D = VGroup(
@@ -181,14 +196,14 @@ class Flow(MovingCameraScene):
 
         self.remove(funkce)
         self.add(matice_D)
-        self.wait()
+        self.wait(wait_time)
         self.add(obrazek)
-        self.wait()
+        self.wait(wait_time)
         self.play(AnimationGroup(*[Wiggle(_) for _ in vlastni_vektory],Create(surroundingRectangle),lag_ratio=.05))
 
-        self.wait()
+        self.wait(wait_time)
         self.play(ReplacementTransform(curves,curves_with_D),ReplacementTransform(vectors.copy(),vectors_with_D),FadeToColor(vectors.set_opacity(.5),GRAY))
-        self.wait()
+        self.wait(wait_time)
 
         first_time = True
         # Moving camera from https://www.youtube.com/watch?v=QTlZp8tiql4
@@ -196,15 +211,53 @@ class Flow(MovingCameraScene):
             self.camera.frame.save_state()
             detail_frame = SurroundingRectangle(detail, color=PURE_RED, buff=.5)
             self.play(Create(detail_frame))
-            self.wait(2)
+            self.wait(2*wait_time)
             self.play(self.camera.frame.animate.set(width=detail_frame.width*2).move_to(detail), running_time = 2)
             if first_time:
                 self.play(FadeOut(detail))
                 self.wait()
                 self.play(FadeIn(detail))
                 first_time = False
-            self.wait(5)
+            self.wait(5*wait_time)
             self.play(Restore(self.camera.frame),FadeOut(detail_frame), running_time = 2)   
-            self.wait()     
+            self.wait(2*wait_time)     
 
         self.wait()
+
+komentar = """
+
+Dobrý den, v tomto videu si ukážeme, jak se přepočítává pomocí gradientu a
+součinitele tepelné vodivosti teplotní spád na tok tepla. Ukážeme si některé
+záludnosti na které narazíme, pokud budeme studovat materiál, mající v různých
+směrech různé vlastnosti. Nejběžnějším představitelem takového materiálu je ten
+nejhezčí a nejdostupnější, totiž dřevo.
+
+Matematický popis je ale univerzální, stejně funguje spád koncentrace a difuzní
+tok, spád vlhkosti a pohyb vody ve dřevě nebo spád hydraulické hladiny a pohyb
+podzemní vody.
+
+Budeme studovat dvourozměrný materiál s určitým rozložením teploty. Termosnímek
+by mohl vypadat jako na obrazovce. Každé teplotě je přiřazena nějaká barva.
+Toto rozložení teploty definuje podnět, na který příroda raeaguje a snaží se
+vyrovnat tepelnou nerovnováhu tokem tepla. Výsledný tok tepla je souhra tohoto
+podnětu s materiálovými vlastnostmi. Nejprve prozkoumejme tento podnět.
+
+Na obrazovce je příliš mnoho informací. To není vždy ideální. Zkusíme si
+vytáhnout jenom některé izotermy, tedy křivky podél nichž je teplota
+konstantní. Pomocí těchto křivek můžeme identifikovat gradient, tedy vektor
+udávající směr a intezitu růstu teploty. Bílé šipky znázorňující gradient jsou
+kolmé k izotermám a jsou delší tam, kde jsou izotermy nahusto.
+
+Pro potřeby toku nepotřebujeme sledovat růst, ale pokles teploty. Vynásobíme
+tedy gradient minus jedničkou a máme záporně vzatý gradient, ukazující směr
+poklesu. Obrázek však není moc přehledný. Pro vylepšení můžeme všechny šipky
+zkrátit na stejnou délku a informaci o délce gradientu zakódovat do barvy
+šipek.
+
+Všechny šipky jsou teď kolmé na izotermy a směřují do míst s menší teplotou.
+Takto, ve směru šipek, by se dalo do pohybu teplo v materiálech, které mají ve
+všech směrech stejné vlastnosti. Například v kovech.
+
+
+
+"""
