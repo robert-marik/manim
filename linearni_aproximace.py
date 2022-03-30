9a4aee55f7f9bd49136822dcdf978643b5345cd2
Robert Marik <marik@mendelu.cz>
2022-03-30 17:02:35 +0200

    linearni aproximace

--- linearni_aproximace.py
+++ linearni_aproximace.py
@@ -13,7 +13,8 @@ class Obrazek(MovingCameraScene):
         title = Title(r"Lineární aproximace")
         self.play(GrowFromCenter(title))
 
-        ax = Axes(x_range=(-0.8,0.8),y_range=(-0.6,0.6), tips=False)
+        ax = Axes(x_range=(-0.75,0.75,.1),y_range=(-0.6,0.6), tips=False)
+        label = MathTex("0.1").scale(0.5).next_to(ax.c2p(0.1,-0.07,0),DOWN) 
         ax.next_to(title,DOWN)
 
         predpis = VGroup(
@@ -24,22 +25,58 @@ class Obrazek(MovingCameraScene):
         predpis[1].set_color(RED)
         #predpis.next_to(ax.c2p(0,0,0),LEFT, buff=0)        
         predpis.set_z_index(10)
-        predpis.add_background_rectangle() 
-        self.add(predpis)        
-          
+        #predpis.add_background_rectangle() 
+
+
+        postup = VGroup(
+            MathTex(r"E_k=\frac{mc^2}{\sqrt{\displaystyle 1-\frac{v^2}{c^2}}} - mc^2"),
+            MathTex(r"E_k=mc^2\left(\frac{1}{\sqrt{\displaystyle 1-\frac{v^2}{c^2}}} - 1\right)"),
+            MathTex(r"E_k=mc^2 f\left(\frac{v^2}{c^2}\right), \quad f(x)=\frac {1}{\sqrt{1-x}}-1"),
+            MathTex(r"E_k=mc^2 f\left(\frac{v^2}{c^2}\right), \quad f(x)\approx\frac {1}{2}x"),
+            MathTex(r"E_k\approx mc^2 \frac 12 \frac{v^2}{c^2}"),
+            MathTex(r"E_k\approx \frac 12 mv^2")
+        ).scale(0.5).arrange(DOWN).next_to(ax)
+
         graf1 = ax.plot(lambda x: 1/np.sqrt(1-x)-1).set_color(BLUE)
         graf1.set_stroke(width=8)
         graf2 = ax.plot(lambda x: 1/2*x).set_color(RED)
-        self.add(ax,graf1,graf2)
-        zavorka = Line(start=ax.c2p(0,-0.01,0), end=ax.c2p(0.01,-0.01,0))
-        zavorka.set_color(YELLOW)
-        self.add(zavorka)
-        self.wait()
+
+        self.play(FadeIn(predpis[0]))
+        self.play(FadeIn(ax,label),FadeIn(graf1))
 
         self.camera.frame.save_state()
-        self.play(self.camera.frame.animate.scale(0.05).move_to(ax.c2p(0,0,0)))
-        self.remove(predpis)
-        self.add(predpis)
+
+        for i in range(3):
+            if i==0:
+                zvetseni=0.4
+            else:
+                zvetseni=1
+            self.play(
+                FadeIn(postup[i]),
+                self.camera.frame.animate.scale(zvetseni).move_to(postup[i]),
+                Circumscribe(postup[i])
+                )
+            self.play(
+                Circumscribe(postup[i])
+                )
+            self.wait()
+ 
+
+        zavorka = DoubleArrow(start=ax.c2p(0,-0.01,0), end=ax.c2p(0.01,-0.01,0),buff=0)
+        zavorka.set_color(PURPLE)
+
+        self.play(self.camera.frame.animate.scale(0.4).move_to(ax.c2p(0,0,0)))
+        self.play(FadeIn(zavorka))
+        self.play(Wiggle(zavorka))
+        self.wait()
+
+        self.play(FadeIn(graf2))
+        self.wait()
+
+        self.play(Restore(self.camera.frame))
+        self.remove(predpis[0])
+        self.add(predpis[0])
+        self.play(FadeIn(predpis[1]))
         self.wait()
 
         self.play(Restore(self.camera.frame))
