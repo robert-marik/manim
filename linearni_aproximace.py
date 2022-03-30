a8a2d5fc371e3271f5138560ea7f8b77d1c49c15
robert-marik <marik@mendelu.cz>
2022-03-30 22:08:35 +0200

    animace

--- linearni_aproximace.py
+++ linearni_aproximace.py
@@ -35,7 +35,7 @@ class Obrazek(MovingCameraScene):
             MathTex(r"E_k=mc^2 f\left(\frac{v^2}{c^2}\right), \quad f(x)\approx\frac {1}{2}x"),
             MathTex(r"E_k\approx mc^2 \frac 12 \frac{v^2}{c^2}"),
             MathTex(r"E_k\approx \frac 12 mv^2")
-        ).scale(0.5).arrange(DOWN).next_to(ax)
+        ).scale(0.5).arrange(DOWN).next_to(ax, buff=1)
 
         graf1 = ax.plot(lambda x: 1/np.sqrt(1-x)-1).set_color(BLUE)
         graf1.set_stroke(width=8)
@@ -46,14 +46,15 @@ class Obrazek(MovingCameraScene):
 
         self.camera.frame.save_state()
 
+        self.play(self.camera.frame.animate.scale(0.5).move_to(postup[0]))
         for i in range(3):
             if i==0:
-                zvetseni=0.4
+                zdroj=VGroup()
             else:
-                zvetseni=1
+                zdroj=postup[i-1].copy() 
             self.play(
-                FadeIn(postup[i]),
-                self.camera.frame.animate.scale(zvetseni).move_to(postup[i]),
+                TransformMatchingShapes(zdroj,postup[i]),
+                self.camera.frame.animate.move_to(postup[i]),
                 Circumscribe(postup[i])
                 )
             self.play(
@@ -61,11 +62,29 @@ class Obrazek(MovingCameraScene):
                 )
             self.wait()
  
+        t0 = MobjectTable([
+            [Tex(r"Auto na dálnici"),MathTex(r"10^{-14}")],
+            [Tex(r"Šhinkansen"),MathTex(r"9\times 10^{-14}")],
+            [Tex(r"NASA X43"),MathTex(r"4\times 10^{-11}")],
+            [Tex(r"Sonda Pioneer 10"),MathTex(r"2\times 10^{-9}")],
+            ], 
+            col_labels=[VGroup(), MathTex(r"v^2/c^2")],
+            include_outer_lines=True,
+            line_config={'color':BLACK, 'stroke_width':0.1},
+        ).scale(.7).to_edge(UP)
+        nadpis = Tex(r"Jak je velký podíl $v^2/c^2$?")
+        t = VGroup(nadpis,t0).arrange(DOWN, buff=1).next_to(postup, buff=1)
 
-        zavorka = DoubleArrow(start=ax.c2p(0,-0.01,0), end=ax.c2p(0.01,-0.01,0),buff=0)
-        zavorka.set_color(PURPLE)
+        self.add(t)
+        self.play(
+            self.camera.frame.animate.scale(2).move_to(t),
+        )
+        self.wait()
 
-        self.play(self.camera.frame.animate.scale(0.4).move_to(ax.c2p(0,0,0)))
+        zavorka = DoubleArrow(start=ax.c2p(0,-0.01,0), end=ax.c2p(0.01,-0.01,0),buff=0,  max_stroke_width_to_length_ratio=30, max_tip_length_to_length_ratio=0.2)
+        zavorka.set_color(PURPLE).set_stroke(width=2)
+
+        self.play(self.camera.frame.animate.scale(0.1).move_to(ax.c2p(0,0,0)))
         self.play(FadeIn(zavorka))
         self.play(Wiggle(zavorka))
         self.wait()
@@ -79,29 +98,12 @@ class Obrazek(MovingCameraScene):
         self.play(FadeIn(predpis[1]))
         self.wait()
 
-        self.play(Restore(self.camera.frame))
-        self.wait()   
-
-class Ek(MovingCameraScene):
-    def construct(self):
-        postup = VGroup(
-            MathTex(r"E_k=\frac{mc^2}{\sqrt{\displaystyle 1-\frac{v^2}{c^2}}} - mc^2"),
-            MathTex(r"E_k=mc^2\left(\frac{1}{\sqrt{\displaystyle 1-\frac{v^2}{c^2}}} - 1\right)"),
-            MathTex(r"E_k=mc^2 f\left(\frac{v^2}{c^2}\right), \quad f(x)=\frac {1}{\sqrt{1-x}}-1"),
-            MathTex(r"E_k=mc^2 f\left(\frac{v^2}{c^2}\right), \quad f(x)\approx\frac {1}{2}x"),
-            MathTex(r"E_k\approx mc^2 \frac 12 \frac{v^2}{c^2}"),
-            MathTex(r"E_k\approx \frac 12 mv^2")
-        ).scale(0.5).arrange(DOWN)
-
-        self.camera.frame.save_state()
-        self.add(postup)
-        for i in range(len(postup)):
-            if i==0:
-                zvetseni=0.4
-            else:
-                zvetseni=1
+        self.play(self.camera.frame.animate.scale(0.5).move_to(postup[3]))
+        for ii in range(3):
+            i = ii+3
             self.play(
-                self.camera.frame.animate.scale(zvetseni).move_to(postup[i]),
+                TransformMatchingShapes(postup[i-1].copy(),postup[i]),
+                self.camera.frame.animate.move_to(postup[i]),
                 Circumscribe(postup[i])
                 )
             self.play(
@@ -109,5 +111,10 @@ class Ek(MovingCameraScene):
                 )
             self.wait()
 
+        self.play(Restore(self.camera.frame))
+        self.wait()   
+
+        all = Group(*[i for i in self.mobjects])
+        self.play(self.camera.frame.animate.scale(2).move_to(all))
         self.wait()
 
