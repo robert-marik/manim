15645842e2ddfa0178bfa4af605bc3d27fafb9c6
Robert Marik <marik@mendelu.cz>
2022-09-13 14:21:09 +0200

    spojitost

--- spojitost.py
+++ spojitost.py
@@ -41,10 +41,10 @@ class Spojitost(Scene):
         predchozi = None
         colors = [YELLOW,RED,GREEN,BLUE]
         ranges = [
-            [[-2,0],[0,3]],
+            [[-2,0.1],[0,3]],
             [[-11,-0.05],[0.25,11]],
-            [[-11,-0.015],[0.015,11]],
-            [[-2,-0.015],[0.015,3]]
+            [[-11,-0.05],[0.05,11]],
+            [[-2,-0.05],[0.05,3]]
         ]        
         dots = [[],[],[0,2],[1]]
 
@@ -66,9 +66,11 @@ class Spojitost(Scene):
             r"$$\lim_{x\to 0}\frac{x^2+x}{x}=1$$",
             ]
 
-        zapis_ = [r"""\begin{minipage}{4cm}\raggedright Žlutá funkce je spojitá v~nule, 
-           ostatní funkce mají v~nule nespojitost. V~případě modré funkce jde o~odstranitelnou 
-           nespojitost (funkce jde učinit spojitou dodefinováním jedné funkční hodnoty). 
+        zapis_ = [r"""\begin{minipage}{7cm}\raggedright Žlutá funkce je spojitá v~nule. 
+           Ostatní funkce mají v~nule bod nespojitosti. 
+           V~případě modré funkce jde funkci učinit spojitou 
+           dodefinováním jedné funkční hodnoty. Jedná se 
+           o~odstranitelnou nespojitost. 
            \par
            \end{minipage}
            """]
@@ -77,7 +79,7 @@ class Spojitost(Scene):
             Tex(_).scale(0.95).set_color(c) for _,c in zip(zapis_, colors)
             ])
         zapis.arrange(DOWN, aligned_edge=LEFT)
-        zapis.set_z_index(100).set_color(WHITE).add_background_rectangle() 
+        zapis.set_z_index(1000).set_color(WHITE).add_background_rectangle(color=GRAY_BROWN, buff=0.2) 
         zapis.to_corner(UR).shift(DOWN)
 
         #i_ = 0       
@@ -91,7 +93,7 @@ class Spojitost(Scene):
             #i_ = i_ + 1
             dot_ = VGroup()
             for _ in dot:
-                dot_.add(Circle(radius=0.1).set_stroke(width=6).set_color(c).move_to(axes.c2p(0,_)))
+                dot_.add(Circle(radius=0.07).set_stroke(width=6).set_color(c).move_to(axes.c2p(0,_)))
 
             if predchozi is None:
                 self.play(Create(soucasny),FadeIn(dot_))
@@ -103,42 +105,87 @@ class Spojitost(Scene):
             self.wait()
             self.next_section()
 
-        self.wait()   
-        self.next_section()
         self.play(FadeIn(zapis))
         self.wait()   
 
-class Spojitost(Scene):
+
+
+class Limita(Scene):
     def construct(self):
         self.next_section("Nadpis")
         title = Title(r"Ostranitelná nespojitost")
-        title.set_z_index(5).add_background_rectangle(buff=1)
+        title.set_z_index(5).add_background_rectangle(buff=.1)
         self.play(GrowFromCenter(title))            
         #self.wait()
 
 
         axes = Axes(
-            x_range=[-10.5,10.5,1],
-            y_range=[-0.5,1.1,1],
+            x_range=[-8.5,8.5,1],
+            y_range=[-0.1,1.1,1],
             tips = False,
-            y_length=2
-            ).next_to(title, DOWN, buff=.7)        
+            y_length=2,
+            x_length=8
+            ).to_corner(DR, buff=0.7)        
 
         axes2 = axes.copy()
-        axes2.next_to(axes,DOWN)
+        axes2.next_to(axes,UP, buff=1)
 
-        def f(x):
-            return np.sin(x)/x
+        limity = [r"1",r"1",r"{\displaystyle \frac 12}"]
+        limity_num = [1,1,0.5]
+        predpisy = [
+            r"{\displaystyle \frac{\sin x}{x}}",
+            r"{\displaystyle \frac{e^x-1}{x}}",
+            r"{\displaystyle \frac{1-\cos x}{x^2}}"
+        ]
 
-        c = RED
-        graf = axes.plot(f,x_range=[-10,10,0.01]).set_color(c)
-        graf2 = VGroup()
-        graf2.add(axes2.plot(f,x_range=[-10,-0.05,0.01]).set_color(c))
-        graf2.add(axes2.plot(f,x_range=[0.05,10,0.01]).set_color(c))
+        funkce = [
+            lambda x:np.sin(x)/x, 
+            lambda x:(np.exp(x)-1)/x, 
+            lambda x:(1-np.cos(x))/x**2, 
+            ]
+
+        for i in range(3):
+
+            def f(x):
+                return funkce[i](x)
+            limita = limity[i]
+            limita_num = limity_num[i]
+            predpis = predpisy[i]
+
+            c = RED
+            c2 = BLUE
+            graf = axes.plot(f,x_range=[-8,8,0.03]).set_color(c)
+            graf2 = VGroup()
+            graf2.add(axes2.plot(f,x_range=[-8,-0.1,0.01]).set_color(c2))
+            graf2.add(Circle(radius=0.05).set_stroke(width=3).move_to(axes2.c2p(0,limita_num)).set_color(c2))
+            graf2.add(axes2.plot(f,x_range=[0.1,8,0.01]).set_color(c2))
 
 
-        self.add(axes,axes2)
-        self.play(Create(graf))
-        for i in graf2:
-            self.play(Create(i))
-        self.wait()
+            popisek2 = VGroup()
+            popisek2.add(Tex(r"$$y="+predpis+r"$$"))
+            popisek2.add(Tex(r"$$\lim_{x\to 0}"+predpis+r"="+limita+r"$$"))
+            popisek2.set_color(c2).arrange(DOWN, aligned_edge=LEFT).next_to(axes2, LEFT).to_edge(LEFT)
+
+            popisek = VGroup(
+                Tex(r"$$y=\begin{cases}"+limita+r"&\text{pro $x=0$}\\[6pt]"+predpis+r" & \text{jinak} \end{cases}$$").set_color(c),
+            ).arrange(DOWN, aligned_edge=LEFT).add_background_rectangle().next_to(axes, LEFT).to_edge(LEFT)
+
+            self.play(FadeIn(popisek2[0]))
+            self.add(axes2)
+            self.play(Create(graf2[0]))
+            self.add(graf2[1])
+            self.play(Create(graf2[2]))
+            self.play(FadeIn(popisek2[1]))
+
+            self.next_section()
+            self.wait()
+
+            self.play(FadeIn(popisek))
+            self.add(axes)
+            self.play(Create(graf))
+
+            self.next_section()
+            self.wait()
+
+            self.play(FadeOut(axes,axes2,graf,graf2,popisek,popisek2))
+
---
+++ spojitost.sh
@@ -0,0 +1,6 @@
+
+manim --save_sections spojitost.py 
+rm -r Spojitost
+manedit --quick_present_export media/videos/spojitost/1080p60/sections/Spojitost.json --quick_present_export media/videos/spojitost/1080p60/sections/Limita.json
+
+
