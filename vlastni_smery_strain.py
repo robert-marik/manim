ed807ef53880f7b9b781dd03c4da6b490e2c120a
Robert Marik <marik@mendelu.cz>
2022-10-27 12:53:31 +0200

    dyne

--- vlastni_smery_strain.py
+++ vlastni_smery_strain.py
@@ -10,7 +10,7 @@ class PumpkinTransform(Scene):
 
         A = VGroup(
                 Tex("Afinní zobrazení dýně"),
-                Tex("Popis v \"přirozené\" soustavě souřadnic"),
+                Tex("Popis v \"obvyklé\" soustavě souřadnic"),
                 Tex("Popis v soustavě souřadnic respektující vlastnosti zobrazení")
         )
         A[1].set_color(RED)
@@ -18,17 +18,11 @@ class PumpkinTransform(Scene):
         for i in A:
             i.add_background_rectangle().to_edge(UP)
 
-        i=1
-        # self.next_section("Transformace "+str(i))
-        # self.remove(autor[0],autor[1], title)
         for mtr in [
             [[5,2],[2,2]],
         ]:
-            i = i+1
-            # for i in self.mobjects:
-            #     self.remove(i)
             k = Circle(radius=1.0, fill_color=RED, fill_opacity=1)
-            kruh = VGroup(k.copy(),k.copy()).arrange(buff=-1.5)
+            #kruh = VGroup(k.copy(),k.copy()).arrange(buff=-1.5)
             kruh = k
                 
             ctverec = Rectangle(height=1, width=1, color=RED,fill_color=RED, fill_opacity=1
@@ -40,7 +34,6 @@ class PumpkinTransform(Scene):
             pusa.scale(.2).shift(0.4*DOWN)
             teleso = VGroup(kruh,oci,pusa).set_z_index(-5)
             
-            teleso2 = teleso.copy()#.set_color(YELLOW)
             M = Matrix(
                 mtr,
                 element_to_mobject=lambda x: MathTex(round(x,2)),
@@ -60,50 +53,55 @@ class PumpkinTransform(Scene):
                 "stroke_color": WHITE,
                 "stroke_opacity":0.5}
                 ).set_z_index(-1)
-            n2 = NumberPlane(
-                x_range=[-10,10,0.25],
-                y_range=[-10,10,0.25],
-                background_line_style={
-                "stroke_color": TEAL,}).set_z_index(-1)
 
-            teleso0 = teleso.copy()
-
-            self.add(A[0])
-            self.play(FadeIn(teleso0))
+            self.add(A[0], teleso)
             self.wait()
             self.next_section("")
-            #return False
-            self.play(ApplyMatrix(mtr, teleso0), 
+            self.play(ApplyMatrix(mtr, teleso), 
               run_time=2)
             self.wait()
 
             self.next_section("")
-            self.play(
-                FadeOut(teleso0),
-                FadeIn(teleso), FadeIn(M), FadeIn(n))  
-            
-            self.play(Transform(A[0],A[1]))
+
+            inverze = np.linalg.inv(np.array(mtr))
+            self.next_section("")
+            self.moving_mobjects = []
+            self.play(ApplyMatrix(inverze, teleso), run_time=0.5)
+            self.play(FadeIn(M), FadeIn(n),Transform(A[0],A[1]))
             self.play(ApplyMatrix(mtr, teleso), 
-              #ApplyMatrix(mtr, n),
               ApplyMatrix(mtr, n)
               ,run_time=2)
-            #self.moving_mobjects = []
+            self.moving_mobjects=[]
             self.wait()
+
             self.next_section("")
-
-            self.play(FadeOut(n))
-
-            self.play(teleso.animate.set_fill(GRAY, opacity=1).set_stroke(color=GRAY, opacity=1))
-
-            self.play(Transform(A[1],A[2]))
-
-            self.add(n2,teleso2)
-
-            self.play(Rotate(n2,angle=np.arctan(0.5)))
-            self.play(ApplyMatrix(mtr, teleso2), 
-              #ApplyMatrix(mtr, n),
-              ApplyMatrix(mtr, n2)
+            phantom = teleso.copy().set_fill(GRAY, opacity=1).set_stroke(color=GRAY, opacity=1)
+            phantom.set_z_index(-15)
+            self.add(phantom)
+            self.play(ApplyMatrix(inverze, teleso), 
+              ApplyMatrix(inverze, n),run_time=2)
+ 
+            
+            self.play(
+                Rotate(n,angle=np.arctan(0.5), run_time=2)
+            )
+            self.wait(0.1)
+            self.play(
+                FadeToColor(n,YELLOW),
+                Transform(A[1],A[2])
+                )
+            self.play(ApplyMatrix(mtr, teleso), 
+              ApplyMatrix(mtr, n)
               ,run_time=2)
-            #self.apply_matrix(mtr)
             self.play(FadeIn(M2))
+
+            vysledek = VGroup(
+                Tex(r"\textbf{Výhody:}"),
+                Tex("Diagonální matice transformace"),
+                Tex("Elementy zůstávají obdélníkové"),
+            ).arrange(DOWN, aligned_edge=LEFT).add_background_rectangle()
+            vysledek.to_corner(DR)
+            self.play(FadeIn(vysledek))
+
+
             self.wait()
